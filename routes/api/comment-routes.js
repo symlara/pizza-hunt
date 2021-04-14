c246e4247f85615e2ffbebc6731833e63c4743c8
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-04-14 17:57:37 -0500

    added new comment and reply functionality to single pizza page

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
 
 // /api/comments/:pizzaId
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/:pizzaId/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId')
+//  This is a PUT route because technically we're not creating a new reply resource
+.put(addReply)
+.delete(removeComment);
 
-
+router.route('/:pizzaId/:commentId/:replyId')
+.delete(removeReply);
 
 
 module.exports = router;
